fc0d86d1d60ff77031bb96bac8e5ef5183b9012d
Kasowy_Code <maciej.smirski@hotmail.com>
2022-07-08 18:20:47 +0200

    config and properties files

--- src/main/java/com/example/demo/MessagePrinter.java
+++ src/main/java/com/example/demo/MessagePrinter.java
@@ -3,16 +3,22 @@ package com.example.demo;
 import com.example.demo.producer.MessageProducer;
 import org.springframework.stereotype.Service;
 
+import java.util.function.Function;
+
 @Service
 public class MessagePrinter {
     private final MessageProducer messageProducer;
+    private final MessagePrinterConfig printerConfig;
 
-    public MessagePrinter (MessageProducer messageProducer) {
+    public MessagePrinter (MessageProducer messageProducer, MessagePrinterConfig printerConfig) {
         this.messageProducer = messageProducer;
+        this.printerConfig = printerConfig;
     }
 
     public void printMessage() {
         String message = messageProducer.getMessage();
-        System.out.println(message);
+        Function<String, String> decorator = printerConfig.getDecorator();
+        String decoratedMessage = decorator.apply(message);
+        System.out.println(decoratedMessage);
     }
 }
