0193d12ed1e36184cbdbb57a76c32eefb4cb7721
Kasowy_Code <maciej.smirski@hotmail.com>
2022-07-08 18:05:41 +0200

    Configuration Classes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,13 +4,20 @@ import org.apache.logging.log4j.message.Message;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
        ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class);
-        MessagePrinter messagePrinter = context.getBean(MessagePrinter.class);
-        messagePrinter.printMessage();
+       MessageController messageController = context.getBean(MessageController.class);
+       messageController.mainLoop();
+    }
+    @Bean
+    Scanner scanner() {
+        return new Scanner(System.in);
     }
 }
