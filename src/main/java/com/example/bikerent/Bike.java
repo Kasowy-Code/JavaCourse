567cf3287da435823cbf7f6776e7402ef721ef58
Kasowy_Code <maciej.smirski@hotmail.com>
2022-07-10 19:15:26 +0200

    Autoincrement

--- src/main/java/com/example/bikerent/Bike.java
+++ src/main/java/com/example/bikerent/Bike.java
@@ -1,11 +1,14 @@
 package com.example.bikerent;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Bike {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String model;
     private String serialNo;
