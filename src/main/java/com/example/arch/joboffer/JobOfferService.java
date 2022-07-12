19175de05c15d14e1cd8cef0bc747d4d3f3b7f13
Kasowy_Code <maciej.smirski@hotmail.com>
2022-07-12 16:45:48 +0200

    Saving data

--- src/main/java/com/example/arch/joboffer/JobOfferService.java
+++ src/main/java/com/example/arch/joboffer/JobOfferService.java
@@ -1,6 +1,8 @@
 package com.example.arch.joboffer;
 
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -16,5 +18,12 @@ class JobOfferService {
     Optional<JobOfferDto> getOfferById(Long id) {
         return jobOfferRepository.findById(id)
                 .map(jobOfferDtoMapper::map);
+    }
+
+    JobOfferDto saveOffer(JobOfferDto jobOfferDto) {
+        JobOffer jobOfferToSave = jobOfferDtoMapper.map(jobOfferDto);
+        jobOfferToSave.setDateAdded(LocalDateTime.now());
+        JobOffer savedJobOffer = jobOfferRepository.save(jobOfferToSave);
+        return jobOfferDtoMapper.map(savedJobOffer);
     }
 }
