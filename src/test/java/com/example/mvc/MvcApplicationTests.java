5bbf7b94346684387cf8a8d5d3e678fa79a7b214
Kasowy_Code <maciej.smirski@hotmail.com>
2022-07-12 10:53:22 +0200

    MVC and Thymeleaf templates

--- src/test/java/com/example/mvc/MvcApplicationTests.java
+++ src/test/java/com/example/mvc/MvcApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.forms;
+package com.example.mvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FormsApplicationTests {
+class MvcApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
