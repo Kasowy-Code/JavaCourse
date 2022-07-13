67f4e678a86a48331eacc2536234b316e51814db
Kasowy_Code <maciej.smirski@hotmail.com>
2022-07-13 11:26:27 +0200

    Constraint messages from properties files

--- src/main/java/com/example/wholesaler/WholesalerApplication.java
+++ src/main/java/com/example/wholesaler/WholesalerApplication.java
@@ -8,12 +8,14 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
+import java.util.Locale;
 import java.util.Set;
 
 @SpringBootApplication
 public class WholesalerApplication {
 
 	public static void main(String[] args) {
+		Locale.setDefault(new Locale("pl"));
 		ConfigurableApplicationContext context = SpringApplication.run(WholesalerApplication.class, args);
 		Validator validator = context.getBean(Validator.class);
 		Product product1 = new Product("Dell XPS 15", "Laptop 15 calowy z 2021 roku", "PL13243");
@@ -25,7 +27,7 @@ public class WholesalerApplication {
 					.forEach(System.out::println);
 		}
 
-		Equipment equipment1 = new Equipment("iPhone 13", "KARNOW123", "EQ7986");
+		Equipment equipment1 = new Equipment("iPhone 13", "KARNOW123", "EQ798");
 		Set<ConstraintViolation<Equipment>> equipmentConstraintViolations = validator.validate(equipment1);
 		if (!equipmentConstraintViolations.isEmpty()) {
 			System.out.println("Nieprawidłowe wyposażenie, złamane ograniczenia:");
